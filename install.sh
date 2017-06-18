7aa9de2909b6be5131323baec71b4e501518943c
Ivraj <iseerha@college.harvard.edu>
2017-06-18 17:08:33 -0400

    adding rm PWD

--- install.sh
+++ install.sh
@@ -5,17 +5,18 @@ echo "Installing vim and tmux..."
 sudo apt-get install vim
 sudo apt-get install tmux
 
+echo "Reconfiguring vim and tmux..."
+
+rm -f ~/.vimrc && rm -f ~/.tmux.conf
+mv * ~/.vim 
+rm -f $PWD
+cd ~/.vim
+ln -s .vimrc ~/.vimrc
+mv ~/.vim/.tmux.conf ~
+
 echo "Installing Vundle..."
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-
-echo "Reconfiguring config files..."
-
-rm -f ~/.vimrc && rm -f ~/.tmux.conf
-mv $PWD ~/.vim 
-cd ~/.vim
-ln -s .vimrc ~/.vimrc
-mv ~/.vim/.tmux.conf ~
 
 echo "Done."

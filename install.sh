ae3160c62a04576a65c9b64fc544e5a413345071
Ivraj <ivrajs@msn.com>
2017-06-18 20:56:46 +0000

    adjusting install order

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ echo "Installing vim and tmux..."
 sudo apt-get install vim
 sudo apt-get install tmux
 
+echo "Installing Vundle..."
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
 echo "Reconfiguring config files..."
 
 rm -f ~/.vimrc && rm -f ~/.tmux.conf
 mv $PWD ~/.vim 
 ln -s .vimrc ~/.vimrc
 mv ~/.vim/.tmux.conf ~
-
-echo "Installing Vundle..."
-
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
 
 echo "Done."

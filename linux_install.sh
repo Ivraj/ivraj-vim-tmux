09c9cf8d2d481242d62dda93c068d930c6aca0fe
Ivraj Seerha <iseerha@college.harvard.edu>
2017-10-12 22:09:23 -0400

    adding mac script. Still need to test

---
+++ linux_install.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+echo "Installing vim and tmux..."
+
+sudo apt-get -y install vim
+sudo apt-get -y install tmux
+
+echo "Reconfiguring vim and tmux..."
+
+rm -rf ~/.vim rm -f ~/.vimrc && rm -f ~/.tmux.conf
+mkdir ~/.vim
+mv .netrwhist ~/.vim/.netwrhist
+mv .tmux.conf ~/.tmux.conf
+mv .vimrc ~/.vim/.vimrc
+mv * ~/.vim 
+rm -rf $PWD
+cd ~/.vim
+cp .vimrc ~/.vimrc
+
+echo "Setting vim as default editor..."
+
+export EDITOR=vim
+
+echo "Installing Vundle..."
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+echo "Done."
